770c9ded5f9b03691ed12247ea0ed5e33b912f83
Thingerbits <38406263+thingerbits@users.noreply.github.com>
2019-03-25 18:21:43 +0530

    Create Reed-switch-with-2-LED.ino

---
+++ Lesson 9 Magnetic Reed Switch Sensor/Code/Reed-switch-with-2-LED.ino
@@ -0,0 +1,27 @@
+int ledOpen=8;
+int ledClose=10;
+int switchReed=6;
+
+void setup(){
+  pinMode(ledOpen, OUTPUT);
+  pinMode(ledClose, OUTPUT);
+  pinMode(switchReed, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  if (digitalRead(switchReed)==HIGH){
+    digitalWrite(ledOpen, LOW);
+    digitalWrite(ledClose, HIGH);
+    Serial.println("Your Door is Closed");
+  }
+  else {
+    digitalWrite(ledOpen, HIGH);
+    digitalWrite(ledClose, LOW);
+    Serial.println("Your Door is Open");
+  }
+  delay(1);
+}
+
+
